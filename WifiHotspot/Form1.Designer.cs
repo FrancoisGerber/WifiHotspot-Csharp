fd6df3492f2c2a84c2a7dee05a6b90d46dc68ed7
francois gerber <francois gerber>
2017-03-23 10:21:01 +0200

    Project Setup

---
+++ WifiHotspot/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WifiHotspot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(324, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "PCRemoteServerWiFi";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(75, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(324, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "fagerber";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SSID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "WIFI Hotspot";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 94);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WIFI Hotspot";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ WifiHotspot/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WifiHotspot
+{
+    public partial class Form1 : Form
+    {
+        bool connect;
+        ContextMenu cm = new ContextMenu();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+           
+            cm.MenuItems.Add("Start WIFI Hotspot", (sender, e) => button1.PerformClick());
+            cm.MenuItems.Add("Stop WIFI Hotspot", (sender, e) => button1.PerformClick());
+            cm.MenuItems.Add("-");
+            cm.MenuItems.Add("Show", (sender, e) =>
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            });
+            cm.MenuItems.Add("Hide", (sender, e) => this.Visible = false);
+            cm.MenuItems.Add("-");
+            cm.MenuItems.Add("Exit", (sender, e) => Application.Exit());
+            notifyIcon1.ContextMenu = cm;
+
+            cm.MenuItems[1].Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ssid = textBox1.Text, key = textBox2.Text;
+            if (!connect)
+            {
+                if (textBox1.Text == null || textBox1.Text == "")
+                {
+                    MessageBox.Show("SSID cannot be left blank !",
+                    "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+
+                    if (textBox2.Text == null || textBox2.Text == "")
+                    {
+                        MessageBox.Show("Key value cannot be left blank !",
+                        "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        if (key.Length >= 6)
+                        {
+                            Zedfi_Hotspot(ssid, key, true);
+                            textBox1.Enabled = false;
+                            textBox2.Enabled = false;
+                            button1.Text = "Stop";
+                            notifyIcon1.BalloonTipText = "WIFI Hotspot is running...";
+                            notifyIcon1.ShowBalloonTip(5000);
+                            cm.MenuItems[0].Visible = false;
+                            cm.MenuItems[1].Visible = true;
+                            connect = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Key should be more then or Equal to 6 Characters !",
+                            "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Zedfi_Hotspot(null, null, false);
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                button1.Text = "Start";
+                notifyIcon1.BalloonTipText = "WIFI Hotspot stopped.";
+                notifyIcon1.ShowBalloonTip(5000);
+                cm.MenuItems[0].Visible = true;
+                cm.MenuItems[1].Visible = false;
+                connect = false;
+            }
+        }
+
+
+        private void Zedfi_Hotspot(string ssid, string key, bool status)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe");
+            processStartInfo.RedirectStandardInput = true;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+            processStartInfo.UseShellExecute = false;
+            Process process = Process.Start(processStartInfo);
+
+            if (process != null)
+            {
+                if (status)
+                {
+                    process.StandardInput.WriteLine("netsh wlan set hostednetwork mode=allow ssid = " + ssid + " key = " + key);
+                    process.StandardInput.WriteLine("netsh wlan start hosted network");
+                    process.StandardInput.Close();
+                }
+                else
+                {
+                    process.StandardInput.WriteLine("netsh wlan stop hostednetwork");
+                    process.StandardInput.Close();
+                }
+            }
+        }
+
+
+        public static bool IsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal p = new WindowsPrincipal(id);
+            return p.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        public void RestartElevated()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = System.Windows.Forms.Application.ExecutablePath;
+            startInfo.Verb = "runas";
+            try
+            {
+                Process p = Process.Start(startInfo);
+            }
+            catch
+            {
+
+            }
+
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!IsAdmin())
+            {
+                RestartElevated();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Zedfi_Hotspot(null, null, false);
+            Application.Exit();
+        }
+    }
+}
